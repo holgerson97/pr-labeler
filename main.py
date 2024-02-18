396902fbaebe06c8a5f0ea8799f0d560041c2e82
Nils Carstensen <me@nils-carstensen.de>
2024-02-18 23:24:14 +0100

    Added: calc and basic GitHub Logic

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from git import Repo
 import os
+from pydriller.metrics.process.lines_count import LinesCount
 
 def get_diff_stats(repo_path: str, base: str, head: str) -> int:
-    repository = Repo(repo_path)
+    metric = LinesCount(path_to_repo=repo_path,
+                        from_commit=base,
+                        to_commit=head)
 
-    base_ref = repository.commit(base)
-    head_ref = repository.commit(head)
-    diffs = base_ref.diff(head_ref)
+    changed_lines = 0
 
-    stats = diffs
+    for lines in metric.count().values():
+        changed_lines += lines
 
-    return stats
+    return changed_lines
+
+def add_label_to_pr(line_count: int):
+    session = requests.Session()
+    session.auth = (os.getenv("GITHUB_ACTOR"), os.getenv("GITHUB_API_TOKEN"))
+   
+    pr_number = os.getenv("GITHUB_REPOSITORY").split('/', 1)[0]
+    url = os.getenv("GITHUB_API_URL") + "/repos/" +  os.getenv("GITHUB_REPOSITORY") + "/issues/" + pr_number + "/labels"
+
 
 def main():
     lines_changed = get_diff_stats("./", os.getenv("GITHUB_BASE_REF"), os.getenv("GITHUB_HEAD_REF"))
